061bc22d1b69615022c33f5452ca0dd86380da06
Jake Kaupp <jkaupp@gmail.com>
2016-05-12 13:22:54 -0400

    Pruning

--- oncat_framework_app/ui.R
+++ oncat_framework_app/ui.R
@@ -1,14 +1,14 @@
 library(shiny)
 
-shinyUI(navbarPage("ONCAT Transferability Framework", theme = "oncat.css",
-  
-               
+shinyUI(navbarPage("ONCAT Transferability Framework", theme = "oncat.css", 
+                   
   # Embedded googleform rendered server-side
   tabPanel("Evaluate Questions",
            htmlOutput("googleForm")),
   
   # Visualization dropdown
   navbarMenu("Visualizations",
+             
              tabPanel("Question 1",
                       plotOutput("q1", height = 1000)
                       ),
@@ -22,6 +22,5 @@ shinyUI(navbarPage("ONCAT Transferability Framework", theme = "oncat.css",
                       )
              
              )
-              
 ))
   
